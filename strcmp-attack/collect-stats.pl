84de2766b271ce4a5b17ccf265aa0efa8cf6d1fa
Ivan Oleynikov <ivan.oleynikov95@gmail.com>
2017-03-09 21:29:39 +0300

    Add header to generated CSV file

--- strcmp-attack/collect-stats.pl
+++ strcmp-attack/collect-stats.pl
@@ -4,6 +4,8 @@ use strict;
 
 my ($its, $maxlen) = @ARGV;
 
+print "iterations,length,cycles\n";
+
 for (my $len = 0; $len < $maxlen; ++$len) {
 	my $str = "a" x $len . "b";
 	my $cycles = `./vulnerable $str $its`;
