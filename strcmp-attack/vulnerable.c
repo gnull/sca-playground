006d221d3e25c9df3d818ee3c171be9aed0fb115
Ivan Oleynikov <io@metrotek.spb.ru>
2017-03-22 18:23:02 +0300

    Rewrite timestamp function with an example from Intel docs

--- strcmp-attack/vulnerable.c
+++ strcmp-attack/vulnerable.c
@@ -3,16 +3,26 @@
 #include <stdint.h>
 #include <string.h>
 
-/* This function is based on similar function written by Bernstein. See
- * `../bernstein-attack-2005/server.c`.
+/* This function is based on examples from Optimization Manual by Intel:
+ *    http://www.intel.com/content/dam/www/public/us/en/documents/white-papers/ia-32-ia-64-benchmark-code-execution-paper.pdf
  *
- * http://www.intel.com/content/dam/www/public/us/en/documents/white-papers/ia-32-ia-64-benchmark-code-execution-paper.pdf
+ * It uses CPUID instruction to ensure that the CPU doesn't reorder
+ * instructions that precede the CPUID with ones that follow it.
  */
 static inline uint64_t timestamp(void)
 {
 	uint32_t bottom;
 	uint32_t top;
-	asm volatile (".byte 15;.byte 49":"=a" (bottom), "=d"(top));
+	asm volatile (
+			"CPUID\n\t"
+			"RDTSC\n\t"
+			"mov %%edx, %0\n\t"
+			"mov %%eax, %1\n\t": "=r" (top), "=r" (bottom)
+#if __x86_64__
+			:: "%rax", "%rbx", "%rcx", "%rdx");
+#elif __i386__
+			:: "%eax", "%ebx", "%ecx", "%edx");
+#endif
 	return (((uint64_t) top) << 32) | bottom;
 }
 
